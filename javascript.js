6d7f95287d526b1e129f35c2188b1041f11aab48
odinstudent <34312554+Alpattex@users.noreply.github>
2023-09-26 12:19:24 +0300

    Call the game function to start the game

--- javascript.js
+++ javascript.js
@@ -4,22 +4,33 @@ function game() {
 
     for (let round = 1; round <= 5; round++) {
         const playerSelection = prompt("Enter your choice (Rock, Paper, or Scissors):");
-        const computerSelection = get ComputerChoice();
+        const computerSelection = getComputerChoice();
 
-    console.log("Round " + round + ":");
-    console.log("Player chose: " + playerSelection);
-    console.log(Computer chose: " + computerSelection");
+        console.log("Round " + round + ":");
+        console.log("Player chose: " + playerSelection);
+        console.log("Computer chose: " + computerSelection);
 
-    const result = playRound(playerSelection, computerSelection);
-    console.log(result);
+        const result = playRound(playerSelection, computerSelection);
+        console.log(result);
 
-    if (result.includes("Win")) {
-        playerScpre++;
-    } else if (result.includes("Lose")) {
-        computerScore++;
+        if (result.includes("Win")) {
+            playerScore++;
+        } else if (result.includes("Lose")) {
+            computerScore++;
+        }
+    }
+
+    console.log("Game Over!");
+    console.log("Player Score: " + playerScore);
+    console.log("Computer Score: " + computerScore);
+
+    if (playerScore > computerScore) {
+        console.log("Congratulations! You win the game!");
+    } else if (computerScore > playerScore) {
+        console.log("You lose the game. Better luck next time!");
+    } else {
+        console.log("It's a tie! The game ends in a draw.");
     }
 }
 
-console.log("Game Over!");
-console.log("Player Score: " + playerScore);
-console.log("Computer Score: " + computerScore);
+game();
