d29fe97e54add2b82fde5c984ccb438b4c714390
odinstudent <34312554+Alpattex@users.noreply.github>
2023-09-26 12:21:24 +0300

    functions were deleted. now Added again

--- javascript.js
+++ javascript.js
@@ -1,3 +1,37 @@
+function getComputerChoice() {
+    // Generate a random number between 0 and 2
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    // Use the random number to determine the computer's choice
+    switch (randomNumber) {
+        case 0:
+            return 'Rock';
+        case 1:
+            return 'Paper';
+        case 2:
+            return 'Scissors';
+    }
+}
+
+// Define the playRound function
+function playRound(playerSelection, computerSelection) {
+    // Convert playerSelection to lowercase for case-insensitive comparison
+    const playerChoice = playerSelection.toLowerCase();
+
+    // Determine the winner based on player's choice and computer's choice
+    if (playerChoice === computerSelection.toLowerCase()) {
+        return "It's a tie! " + playerSelection + " equals " + computerSelection;
+    } else if (
+        (playerChoice === "rock" && computerSelection === "scissors") ||
+        (playerChoice === "paper" && computerSelection === "rock") ||
+        (playerChoice === "scissors" && computerSelection === "paper")
+    ) {
+        return "You Win! " + playerSelection + " beats " + computerSelection;
+    } else {
+        return "You Lose! " + computerSelection + " beats " + playerSelection;
+    }
+}
+
 function game() {
     let playerScore = 0;
     let computerScore = 0;
