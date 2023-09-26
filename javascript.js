0f900c6a2f87ccc07515605b010dce4e5128ac2b
odinstudent <34312554+Alpattex@users.noreply.github>
2023-09-26 11:40:49 +0300

    Fixed getComputerChoice "rnadom" to random typos

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ function getComputerChoice() {
     // Generate a random number between 0 and 2
     const randomNumber = Math.floor(Math.random() * 3);
 
-    // Use the rnadom number to determine the computer's choice
+    // Use the random number to determine the computer's choice
     switch (randomNumber) {
         case 0:
             return 'Rock';
@@ -16,3 +16,27 @@ function getComputerChoice() {
 // Example usage:
 const computerChoice = getComputerChoice();
 console.log('Computer chose: ' + computerChoice);
+
+function playRound(playerSelection, computerSelection) {
+    // Convert playerSelection to lowercase for case-insensitive comparison
+    const playerChoice = playerSelection.toLowerCase();
+
+    // Determine the winner based on player's choice and computer's choice
+    if (playerChoice === computerSelection.toLowerCase()) {
+        return "It's a tie! " + playerSelection + " equals " + computerSelection;
+    } else if (
+        (playerChoice === "rock" && computerSelection === "scissors") ||
+        (playerChoice === "paper" && computerSelection === "rock") ||
+        (playerChoice === "scissors" && computerSelection === "paper")
+    ) {
+        return "You Win! " + playerSelection + " beats " + computerSelection;
+    } else {
+        return "You Lose! " + computerSelection + " beats " + playerSelection;
+    }
+}
+
+// Example usage:
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice(); // Assuming you have the getComputerChoice function from previous responses.
+const result = playRound(playerSelection, computerSelection);
+console.log(result);
